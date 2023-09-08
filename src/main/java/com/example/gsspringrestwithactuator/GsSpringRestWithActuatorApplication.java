95472ae796af5601d8392bf1f99a816a172cad5f
alamnr <alamnr@gmail.com>
2023-09-08 13:08:20 +0600

    Initial commit

---
+++ src/main/java/com/example/gsspringrestwithactuator/GsSpringRestWithActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsspringrestwithactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSpringRestWithActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsSpringRestWithActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gsspringrestwithactuator/GsSpringRestWithActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsspringrestwithactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSpringRestWithActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
